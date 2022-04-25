ec0aa3fce71346a6da3fe17c641f551ba98f9102
Marcos Andrade <marcospandrade2@gmail.com>
2022-04-25 09:20:28 -0300

    build: specification services + routes

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,1 +1,11 @@
-export class ISpecificationRepository {}
+import { Specification } from "../model/Specification";
+
+export interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+export interface ISpecificationsRepository {
+  create({ name, description }: ICreateSpecificationDTO): void;
+  findByName(name: string): Specification;
+}
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,24 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 export class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const SpecificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (SpecificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
